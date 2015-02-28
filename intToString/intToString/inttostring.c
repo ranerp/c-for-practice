5b0eb06f894304c7dfc0f5e6501bdb7935f9df6b
twtChnz <raner.piibur@gmail.com>
2015-02-28 13:20:58 +0200

    practice

--- intToString/intToString/inttostring.c
+++ intToString/intToString/inttostring.c
@@ -4,18 +4,18 @@
 
 #include "inttostring.h"
 
-const char* numbersPtr[] = { "", "one", "two", "three", "four", "five", "six", "seven", "height", "nine", "ten",
+const char* NUMBERS_PTR[] = { "", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
 				   "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "nineteen"};
 
-const char* tensPtr[] = { "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
+const char* TENS_PTR[] = { "", "", "twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety" };
 
-const char* powersPtr[] = { "", "thousand", "million", "billion" };
+const char* POWERS_PTR[] = { "", "thousand", "million", "billion" };
 
-const char negative[] = "minus";
+const char NEGATIVE[] = "minus";
 
-const char hundred[] = "hundred";
+const char HUNDRED[] = "hundred";
 
-char fill[] = " ";
+const char FILL[] = " ";
 
 int main()
 {
@@ -28,13 +28,73 @@ int main()
 		printf("Error allocating memory\n");
 		return 1;
 	}
-
-	strcpy(answerPtr, "z");
+	strcpy(answerPtr, "");
 	
 	printf("Enter an integer to convert it to string\n");
 	scanf("%d", &integer);
 
-	printf("%s", answerPtr);
+	if (integer < 0)
+	{
+		integer *= -1;
+		addstr(&answerPtr, NEGATIVE);
+		addstr(&answerPtr, FILL);
+	}
+
+	double base_ten = (double)floor(log10(integer));
+	int i = (int)ceil(base_ten / 3);
+	int base = fmax(pow(1000, i), 1000);
+
+	while (base > 1)
+	{		
+		int hundreth = integer % base;
+		int tenth = hundreth % (base / 10);
+		int oneth = tenth % (base / 100);
+
+		int thousand = integer / base;
+		int hundred = hundreth / (base / 10);
+		int ten = tenth / (base / 100);
+		int one = oneth / (base / 1000);
+
+		integer -= one * (base / 1000) + ten * (base / 100) + hundred * (base / 10) + thousand * base;
+
+		if (thousand > 0)
+		{
+			add_number(&answerPtr, thousand);
+			addstr(&answerPtr, FILL);
+
+			addstr(&answerPtr, POWERS_PTR[i]);
+			addstr(&answerPtr, FILL);
+		}
+
+		add_hundred(&answerPtr, hundred);
+		addstr(&answerPtr, FILL);
+
+		if (ten == 1)
+		{
+			add_number(&answerPtr, 10 + one);
+			addstr(&answerPtr, FILL);
+		}
+		else
+		{
+			add_tens(&answerPtr, ten);
+			addstr(&answerPtr, FILL);
+
+			add_number(&answerPtr, one);
+			addstr(&answerPtr, FILL);
+		}
+		
+		if (ten != 0  || one != 0 || hundred != 0)
+		{
+
+			addstr(&answerPtr, POWERS_PTR[i - 1]);
+			addstr(&answerPtr, FILL);
+		}
+
+		base /= 1000;
+		i--;
+	}
+
+	printf("ANSWER: %s\n", answerPtr);
 
 	getch();
 
@@ -42,7 +102,30 @@ int main()
 	return 0;
 }
 
-int addstr(char **destination, char *source)
+void add_hundred(char **destination, int integer)
+{
+	int ptr = fmin(integer, 9);
+	if (integer > 0)
+	{
+		addstr(destination, NUMBERS_PTR[ptr]);
+		addstr(destination, FILL);
+		addstr(destination, HUNDRED);
+	}
+}
+
+void add_tens(char **destination, int integer)
+{
+	int ptr = fmin(integer, 18);
+	addstr(destination, TENS_PTR[ptr]);
+}
+
+void add_number(char **destination, int integer)
+{
+	int ptr = fmin(integer, 18);
+	addstr(destination, NUMBERS_PTR[ptr]);
+}
+
+void addstr(char **destination, char *source)
 {
 	size_t len = strlen(*destination) + strlen(source) + sizeof(char);
 
@@ -54,7 +137,7 @@ int addstr(char **destination, char *source)
 	}
 	else
 	{
-		printf("Could not reallocate memory to add string");
+		printf("Could not reallocate memory to concatenate string");
 	}
 }
 
--- intToString/intToString/inttostring.h
+++ intToString/intToString/inttostring.h
@@ -1,1 +1,7 @@
-int addstr(char **deststr, char *addstr);
+void add_hundred(char **destination, int integer);
+
+void add_tens(char **destination, int integer);
+
+void add_number(char **destination, int integer);
+
+void addstr(char **destination, char *source);
